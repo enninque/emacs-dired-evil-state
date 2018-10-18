c3d89b614d2594f1fc9111b199a53c4d7a17a723
En Quer <en.quer.open@protonmail.com>
2018-10-18 19:55:18 +0300

    Initial commit.

---
+++ dired-evil-state.el
@@ -0,0 +1,44 @@
+;;; dired-evil-state.el --- 
+;;
+;; Author:  <enquer@sabayon.local>
+;; Version: 0.1
+;; URL: 
+;; Keywords:
+;; Compatibility:
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Commentary:
+;; .
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 2, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Code:
+
+(require 'cl-lib)
+(require 'evil)
+
+(evil-define-state dired
+  "State for dired."
+  :tag "<Dired>"
+  :suppress-keymap t)
+
+(provide 'dired-evil-state)
+;;; dired-evil-state.el ends here
